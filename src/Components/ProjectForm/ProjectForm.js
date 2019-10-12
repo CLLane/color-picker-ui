9eb929f7e3eab70b0494f099ef1d875cb6738bde
Djavan Munroe <djavanm24@gmail.com>
2019-10-12 13:25:18 -0600

    Fixed logic on inputting new project

--- src/Components/ProjectForm/ProjectForm.js
+++ src/Components/ProjectForm/ProjectForm.js
@@ -8,7 +8,8 @@ export class ProjectForm extends Component {
       palette: null,
       isExistingProject: false,
       projectToSave: '',
-      paletteToSave: ''
+      paletteToSave: '',
+      selectValue: 'new-project'
     }
   };
 
@@ -16,9 +17,9 @@ export class ProjectForm extends Component {
     const { projects } = this.props;
     let currentProject = projects.find(project => project.name === value);
     if ( currentProject ) {
-      this.setState({ projectToSave: currentProject, isExistingProject: true })
+      this.setState({ projectToSave: currentProject, isExistingProject: true, selectValue:value })
     } else {
-      this.setState({ projectToSave: "", isExistingProject: false})
+      this.setState({ projectToSave: "", isExistingProject: false, selectValue: 'new-project'})
     }
   }
 
@@ -26,27 +27,28 @@ export class ProjectForm extends Component {
     this.setState({ [e.target.name]: e.target.value })
   }
 
-  handleSubmit = (e) => { 
+  handleSubmit = async (e) => { 
     e.preventDefault();
     const { projectToSave, paletteToSave } = this.state;
     const { handleSubmission } = this.props;
-    handleSubmission(projectToSave, paletteToSave);
-    this.setState({ paletteToSave: "" });
+    await handleSubmission(projectToSave, paletteToSave);
+    projectToSave.name ? this.setState({ selectValue: projectToSave.name, isExitingProject: true}) : this.setState({ selectValue: projectToSave, isExistingProject: true});
+    this.setState({ projectToSave: "", paletteToSave: ""});
   }
 
   render() {
     const { projects } = this.props;
-    const { isExistingProject, projectToSave, paletteToSave } = this.state;
+    const { isExistingProject, projectToSave, paletteToSave, selectValue } = this.state;
     const projectPlaceholderText = isExistingProject ? projectToSave.name : 'Enter Project Name';
-    const projectNameValue = isExistingProject ? '' : projectToSave.name;
+    const projectNameValue = isExistingProject ? projectToSave.name : projectToSave;
     const options = projects.map((project, index) => {
       return <option key={index} value={project.name}>{project.name}</option>
-    });
+    })
     return (
       <form>
-        <select onChange={(e) => this.selectProject(e.target.value)}>
-          <option value='new-project'>Create New Project</option>
+        <select value={selectValue} onChange={(e) => this.selectProject(e.target.value)}>
           { options }
+          <option value='new-project'>Create New Project</option>
         </select>
         <input type='text' disabled={isExistingProject} name='projectToSave' placeholder={ projectPlaceholderText } onChange={this.handleNameChange} value={projectNameValue}></input>
         <input type='text' placeholder='Enter Palette Name' name='paletteToSave' onChange={this.handleNameChange} value={paletteToSave}></input>
