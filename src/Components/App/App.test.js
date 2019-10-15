2a8da14ff6969df18db6913d3eef79bd7e8f2518
Djavan Munroe <djavanm24@gmail.com>
2019-10-15 08:33:47 -0600

    Set up snapshot and initial test for App

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow, maount } from 'enzyme';
+import { MemoryRouter } from 'react-router'
 import App from './App';
+import './App.css';
+import LoginForm from "../LoginForm/LoginForm";
+import ColorContainer  from "../ColorContainer/ColorContainer";
+import ProjectForm from "../ProjectForm/ProjectForm";
+import ProjectsContainer from "../ProjectsContainer/ProjectsContainer";
+import PaletteContainer from "../PaletteContainer/PaletteContainer";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+  
+  it('Should match the snapshot with the proper data passed in.', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
 });
