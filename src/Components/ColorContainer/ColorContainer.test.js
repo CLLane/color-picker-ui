c6f4ad85acf0d27b74b1c2ce8c84f20220d4e677
Djavan Munroe <djavanm24@gmail.com>
2019-10-15 20:37:45 -0600

    Add test and snapshot for color container

---
+++ src/Components/ColorContainer/ColorContainer.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import './ColorContainer.css';
+import ColorContainer from './ColorContainer';
+
+describe('Color Container', () => {
+  let wrapper;
+  let mockColors;
+  let mockGenerateColors;
+  let mockToggleColorLock;
+  beforeEach(() => {
+    mockColors = [
+      { color: '#FFFFFF', locked: false },
+      { color: '#FFFFFF', locked: false },
+      { color: '#FFFFFF', locked: false },
+      { color: '#FFFFFF', locked: false },
+      { color: '#FFFFFF', locked: false }
+    ];
+    mockGenerateColors = jest.fn();
+    mockToggleColorLock = jest.fn();
+    wrapper = shallow(<ColorContainer 
+      colors={mockColors}
+      generateColors={mockGenerateColors}
+      toggleColorLock={mockToggleColorLock}
+    />);
+  });
+
+  it('should match the snapshot, given a set of colors', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should call the generate colors function when clicked', () => {
+    wrapper.find('button').simulate('click');
+    expect(mockGenerateColors).toHaveBeenCalled();
+  });
+});
